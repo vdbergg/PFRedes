3fa7e515f9bdbb64b752c6c450bbfaba2b785a56
Berg <vandenberg.jutai@gmail.com>
2017-03-23 18:16:12 -0400

    Chat funcionando com login gmail

--- app/src/main/java/com/berg/pfredes/PlayerActivity.java
+++ app/src/main/java/com/berg/pfredes/PlayerActivity.java
@@ -1,6 +1,8 @@
 package com.berg.pfredes;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -216,8 +218,9 @@ public class PlayerActivity extends AppCompatActivity implements GoogleApiClient
 
                 //Get references to the views of list_item.xml
                 TextView messageText, messageUser, messageTime;
-                CircleImageView photoUser;
-                photoUser = (CircleImageView) v.findViewById(R.id.photo_user);
+                ImageView photoUser;
+
+                photoUser = (ImageView) v.findViewById(R.id.photo_user);
                 messageText = (EmojiconTextView) v.findViewById(R.id.message_text);
                 messageUser = (TextView) v.findViewById(R.id.message_user);
                 messageTime = (TextView) v.findViewById(R.id.message_time);
@@ -229,7 +232,7 @@ public class PlayerActivity extends AppCompatActivity implements GoogleApiClient
                         .placeholder(R.drawable.com_facebook_profile_picture_blank_square)
                         .crossFade()
                         .into(photoUser);
-
+                
                 messageText.setText(model.getMessageText());
                 messageUser.setText(model.getMessageUser());
                 messageTime.setText(DateFormat.format("HH:mm", model.getMessageTime()));
